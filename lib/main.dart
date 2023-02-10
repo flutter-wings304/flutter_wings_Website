48f6f4e1c19c43de53c7ba712f2e59f5b565fd83
risheeta jain <risheetajain309@gmail.com>
2023-02-10 22:33:05 +0530

    base url in log file

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_wings_website/pdfviewer.dart';
 import 'package:flutter_wings_website/welcome.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'autufillhints.dart';
-import 'blog_list.dart';
-import 'listequals.dart';
-import 'new_screen.dart';
-
 Future main() async {
-   await dotenv.load(fileName: "lib/.env");
+  await dotenv.load(fileName: "lib/.env");
+  var url = window.location;
+  print(url);
   runApp(const MyApp());
 }
 
@@ -22,22 +20,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Title of Your App',
-      
       theme: ThemeData(
-        // fontFamily: GoogleFonts.notoSans(),
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        fontFamily: "Noto"
-      ),
-      home: const BlogListScreen(),
+          primarySwatch: Colors.blue,
+          fontFamily: GoogleFonts.notoSans().fontFamily),
+      home: const Welcomescreen(),
     );
   }
 }
@@ -61,7 +47,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final int _counter = 0;
 
   void _incrementCounter() {
     print(dotenv.env['VAR_NAME']);
@@ -73,15 +59,12 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-       
-      
-      ),
+      body: const Center(),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
